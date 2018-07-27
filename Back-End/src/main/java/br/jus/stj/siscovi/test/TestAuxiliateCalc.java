864d2573a2c34f98afe74b1d089d744d95a56780
Vinícius de Sousa Santana <sousavinicius@hotmail.com>
2018-07-27 13:04:12 -0300

    Arquivos atualizados.

--- Back-End/src/main/java/br/jus/stj/siscovi/test/TestAuxiliateCalc.java
+++ Back-End/src/main/java/br/jus/stj/siscovi/test/TestAuxiliateCalc.java
@@ -51,7 +51,9 @@ public class TestAuxiliateCalc {
             System.out.println(date.toLocalDate().format(DateTimeFormatter.ofPattern("cccc, dd 'de' "+ "MMMM" +" 'de' uuuu")));
             connectSQLServer.dbConnect().close();
             System.out.println(date.toLocalDate().getDayOfMonth());
-            System.out.print(ChronoUnit.DAYS.between(Date.valueOf("2017-07-01").toLocalDate(), Date.valueOf("2017-12-31").toLocalDate()));
+            System.out.println(date1.toLocalDate().getMonthValue());
+            System.out.println(ChronoUnit.DAYS.between(Date.valueOf("2017-07-01").toLocalDate(), Date.valueOf("2017-07-02").toLocalDate()));
+
         } catch (SQLServerException sqlse) {
             sqlse.printStackTrace();
         } catch(SQLException sqle){
